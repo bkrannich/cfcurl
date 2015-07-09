a460d2b3f811a4340a7292f934059fcfdddf114a
krujos <jkruck@pivotal.io>
2015-07-09 14:22:14 -0600

    Change to return something consistent with the json package

--- cfcurl.go
+++ cfcurl.go
@@ -4,11 +4,11 @@ import "errors"
 
 // Curl calls cf curl  and return the resulting json. This method will fail if
 // the api is depricated
-func Curl(path string) (string, error) {
-	return "", errors.New("Not Implemented")
+func Curl(path string) (map[string]interface{}, error) {
+	return nil, errors.New("Not Implemented")
 }
 
 // CurlDepricated calls cf curl and return the resulting json, even if the api is depricated
-func CurlDepricated(path string) (string, error) {
-	return "", errors.New("Not implemented")
+func CurlDepricated(path string) (map[string]interface{}, error) {
+	return nil, errors.New("Not implemented")
 }
